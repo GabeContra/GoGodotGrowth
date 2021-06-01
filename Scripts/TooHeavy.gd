6220faa832521ccbac3957a12de1db5d39d10012
Damian Leyva <dleyva.hov@gmail.com>
2021-06-01 15:44:46 -0600

    Added up to Level 5

--- Scripts/TooHeavy.gd
+++ Scripts/TooHeavy.gd
@@ -5,7 +5,9 @@ extends Popup
 
 func _on_Ball_tooHeavy():
 	popup()
-	$Tween.interpolate_property(self, "anchor_top", anchor_top, 0.8, 0.2, Tween.TRANS_LINEAR)
 	$Tween.stop_all()
+	$Tween.interpolate_property(self, "anchor_top", anchor_top, 0.8, 0.2, Tween.TRANS_LINEAR)
 	$Tween.start()
+
+func _on_Tween_tween_completed(object, key):
 	$Tween.interpolate_property(self, "anchor_top", anchor_top, 1, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 1)
